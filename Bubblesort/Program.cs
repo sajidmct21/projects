1dc38441df2198522176e22de0d84da7dc29fce4
Sajid_yaqub <Sajidmct21@gmail.com>
2021-05-22 14:06:20 +0500

    Time Complexity of bubble sort is done

--- Bubblesort/Program.cs
+++ Bubblesort/Program.cs
@@ -16,6 +16,7 @@ class GFG
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
+        
     }
 
     /* Prints the array */
@@ -25,12 +26,14 @@ class GFG
         for (int i = 0; i < n; ++i)
             Console.Write(arr[i] + " ");
         Console.WriteLine();
+        int tc = n*(n-1)/2;
+        Console.WriteLine("Time Complexity {0}ms", tc);
     }
 
     // Driver method
     public static void Main()
     {
-        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+        int[] arr = { 64, 34, 2, 12, 22, 11, 90 };
         bubbleSort(arr);
         Console.WriteLine("Sorted array");
         printArray(arr);
